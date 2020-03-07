8422a9d214c6917d094d632da77f8098fda8f7e9
Fahad Abbas <adventivepk@gmail.com>
2020-03-07 18:49:13 +0500

    added : round model added

---
+++ app/Models/Rounds.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Exception;
+use DB;
+
+
+class Rounds extends Model 
+{
+    
+
+    protected $table = 'rounds';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+
+
+
+
+    
+}
