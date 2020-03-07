e67db0ada01105feed24ae2f9d5aa6e13d15e6ed
Fahad Abbas <adventivepk@gmail.com>
2020-03-07 16:22:06 +0500

    new migration added

--- app/functions_helper.php
+++ app/functions_helper.php
@@ -1,8 +1,33 @@
 <?php
 
+/**
+ * @method : pr()
+ * @author : adventivepk@gmail.com <Fahad Abbas>
+ * @param  : Null  
+ * @return : formatted array
+ * @since : 07/03/2020
+ * @example : none
+*/
+function pr($array){
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";  
+}
 
-function test(){
 
+/**
+ * @method : pre()
+ * @author : adventivepk@gmail.com <Fahad Abbas>
+ * @param  : Null  
+ * @return : formatted array and exits the code
+ * @since : 07/03/2020
+ * @example : none
+*/
+function pre($array){
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";  
+    exit;
 }
 
 ?>
